0d10d858578a701794d5ecf2da2f315476285854
Sinrath <phi7po@gmail.com>
2020-09-04 14:14:10 +0200

    html

--- web/src/app.ts
+++ web/src/app.ts
@@ -1,17 +1,33 @@
 import express from "express";
-
+import handlebars from "handlebars";
+import path from "path";
+import fs from "fs";
 import { GrovePi } from "../../sensor/src/sensors";
+
+
+const indexFilePath = path.join(__dirname, "../views/index.html");
+const fileContent = fs.readFileSync(indexFilePath, "utf8");
+const template = handlebars.compile(fileContent);
 
 const sensor = new GrovePi(1);
 
 const server = express();
 
+
 server.get("/", function (req, res) {
     
     sensor.measureTemperature()
-    .then((temp) => res.send(`Die Temperatur ist: ${temp}`))
-    .catch(error => console.log(error));
+    .then((temp) => {
+        let appliedValues = template({
+            temperatureNow: temp
+        })
+        console.log(appliedValues);
+        res.send(appliedValues);
+    })
+    
+        .catch(error => console.log(error));
 
+    
 });
 
 
