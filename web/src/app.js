307483c9b7965b157ae3d8332a36063070d3b773
Sinrath <phi7po@gmail.com>
2020-09-04 13:29:03 +0200

    web

---
+++ web/src/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var sensors_1 = require("../../sensor/src/sensors");
+var sensor = new sensors_1.GrovePi(1);
+var server = express_1.default();
+server.get("/", function (req, res) {
+    sensor.measureTemperature()
+        .then(function (temp) { return res.send("Die Temperatur ist: " + temp); })
+        .catch(function (error) { return console.log(error); });
+});
+server.listen(3000, function () {
+    console.log("Server listening on port 3000!");
+});
---
+++ web/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+import { GrovePi } from "../../sensor/src/sensors";
+
+const sensor = new GrovePi(1);
+
+const server = express();
+
+server.get("/", function (req, res) {
+    
+    sensor.measureTemperature()
+    .then((temp) => res.send(`Die Temperatur ist: ${temp}`))
+    .catch(error => console.log(error));
+
+});
+
+
+server.listen(3000, function (){
+    console.log("Server listening on port 3000!");
+});
