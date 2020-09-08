ed908ccd76119538a4489d42ad37eb02416e0483
usrt0023 <usrt0023@students.zhaw.ch>
2020-09-08 15:28:13 +0200

    added desired date format

--- web/src/database.ts
+++ web/src/database.ts
@@ -1,4 +1,6 @@
 import {Client, Connect} from 'ts-postgres'
+import moment from 'moment';
+import {GrovePi} from "../../sensor/src/sensors";
 
 const client = new Client ({
     user : 'postgres',
@@ -8,20 +10,25 @@ const client = new Client ({
     port : 5432
     });
 
+const sensor = new GrovePi(1);
+
 const datum:Date = new Date();
-console.log(datum)
+
+let formattedDate = (moment(datum)).format('YYYY-MM-DD HH:mm:ss')
+console.log(formattedDate)
     
+let addDatum = "INSERT INTO messung (id, temperatur, datum) VALUES ("+6172+","+sensor.measureTemperature+",'"+formattedDate+"');";
+console.log(addDatum);
+
+
 client.connect().then( (connect: Connect) => {
     console.log(connect);
 }).catch((reason:any) => {
     console.log(reason);  
 });
 
-let queryMessung = 'select * from messung;';
-
-client.query(queryMessung).then((result:any)=>{
+client.query(addDatum).then((result:any)=>{
     console.log(result);
 }).catch((reason:any)=>{
     console.log(reason);
 })
-
